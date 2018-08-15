324bf93e3c7418523bfda749f61030563a066ad5
Brian <40767783+brishaw@users.noreply.github.com>
2018-08-15 14:51:00 -0400

    Formatted game.js with new code, and changed some CSS and HTML...

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,159 +1,97 @@
-$(document).ready(function(){
 
 
-    // set up random number to be shown at the beginning of the game between 19 - 120
+var wins = 0;
 
-    // x = 19, y = 120
-    var randomNum = Math.floor(Math.random() * ((120-19)+1) + 19);
-
-    // show random number on screen
-
-    var randomNumBox = document.getElementById("random-number");
-
-    // display the random number to play against
-    randomNumBox.textContent = randomNum;
-
-
-    // ****************
-
-    var playerNumberBox = document.getElementById("player-number");
-   
-    var playerNumberArray = [];
-
-    // x = 1, y = 12
-    var randomCrystalNum1 = Math.floor(Math.random() * ((12 - 1) + 1) + 1);
-    // x = 1, y = 12
-    var randomCrystalNum2 = Math.floor(Math.random() * ((12 - 1) + 1) + 1);
-    // x = 1, y = 12
-    var randomCrystalNum3 = Math.floor(Math.random() * ((12 - 1) + 1) + 1);
-    // x = 1, y = 12
-    var randomCrystalNum4 = Math.floor(Math.random() * ((12 - 1) + 1) + 1);
-
-    var counter = 0;
-
-    $("#btn1").click(function(){
-
-        alert(randomCrystalNum1);
-        counter += 1;
-        playerNumberBox.textContent = randomCrystalNum1 * counter;
-
-        //var num1 = randomCrystalNum1 += randomCrystalNum1;
-
-        //playerNumberBox.textContent = num1;
-
-        
-        // let x = randomCrystalNum1;
-        // let originalX = x;
-        //console.log(x += originalX); // 20
-        // console.log(x += originalX);
-        //   console.log(x += originalX);
-        
-
-        //playerNumberBox.textContent = num1;
-        
-        
-        // var num1 = $("#btn1").data("value", randomCrystalNum);
-
-        // playerNumberArray.push(randomCrystalNum);
-
-        // console.log(playerNumberArray);
-
-        // console.log($("#btn1").data("value"));
-    
-    })
-    
-    $("#btn2").click(function () {
-        alert(randomCrystalNum2);
-        counter += 1;
-        playerNumberBox.textContent = randomCrystalNum2 * counter;
-    })
-
-    // function button2() {
-        
-        
-           // x = 1, y = 12
-    //     var randomCrystalNum = Math.floor(Math.random() * ((12 - 1) + 1) + 1);
-    //     var num2 = $("#btn2").data("value", randomCrystalNum);
-
-    //     playerNumberArray.push(randomCrystalNum);
-
-    //     console.log(playerNumberArray);
-
-    //     console.log($("#btn2").data("value"));
-    // }
-
-    //button2();
-
-    // function button3() {
-        
-           // x = 1, y = 12
-    //     var randomCrystalNum = Math.floor(Math.random() * ((12 - 1) + 1) + 1);
-    //     var num3 = $("#btn3").data("value", randomCrystalNum);
-        
-    //     playerNumberArray.push(randomCrystalNum);
-
-    //     console.log(playerNumberArray);
-
-    //     console.log($("#btn3").data("value"));
-    // }
-
-    // button3();
-
-    $("#btn3").click(function () {
-        alert(randomCrystalNum3);
-        counter += 1;
-        playerNumberBox.textContent = randomCrystalNum3 * counter;
-    })
-
-    // function button4() {
-        
-           // x = 1, y = 12
-    //     var randomCrystalNum = Math.floor(Math.random() * ((12 - 1) + 1) + 1);
-    //     var num4 = $("#btn4").data("value", randomCrystalNum);
-
-    //     playerNumberArray.push(randomCrystalNum);
-
-    //     console.log(playerNumberArray);
-
-    //     console.log($("#btn4").data("value"));
-    // }
-
-    // button4();
-
-    $("#btn4").click(function () {
-        alert(randomCrystalNum4);
-        counter += 1;
-        playerNumberBox.textContent = randomCrystalNum4 * counter;
-    })
-
-    
-    // ****************
-
-    // $("#btn1").click(function(){
-    //     console.log(playerNumberArray.indexOf[0];
-    //     $(playerNumberBox).textContent = playerNumberArray.indexOf([0]);
-    // })
-   
-    
-    
-    
-       // ****************
-
-    // empty crystal array to hold the 4 random numbers
-
-    // var crystalArray = [];
-
-    // for(i = 0; i < 4; i++) {
-
-        // set up random numbers for crystalArray
-        // x = 1, y = 12
-    //     var randomCrystalNum = Math.floor(Math.random() * ((12 - 1) + 1) + 1);
-    //     crystalArray.push(randomCrystalNum);
-    // }
-    //console.log(crystalArray);
-    //console.log(crystalArray.indexOf(0));
+var losses = 0;
 
 
 
 
-}) // end jquery
+// set up random number to be shown at the beginning of the game between 19 - 120
+
+// x = 19, y = 120
+var randomNum = Math.floor(Math.random() * ((120 - 19) + 1) + 19);
+
+// show random number on screen
+
+$("#random-number").text(randomNum);
+
+
+/*********************************
+ *********************************/
+
+
+var counter = 0;
+// x = 1, y = 12
+var randomCrystalNum1 = Math.floor(Math.random() * ((12 - 1) + 1) + 1);
+// x = 1, y = 12
+var randomCrystalNum2 = Math.floor(Math.random() * ((12 - 1) + 1) + 1);
+// x = 1, y = 12
+var randomCrystalNum3 = Math.floor(Math.random() * ((12 - 1) + 1) + 1);
+// x = 1, y = 12
+var randomCrystalNum4 = Math.floor(Math.random() * ((12 - 1) + 1) + 1);
+
+var numberOptions = [randomCrystalNum1, randomCrystalNum2, randomCrystalNum3, randomCrystalNum4];
+
+
+ console.log(numberOptions);
+
+
+// Next we create a for loop to create crystals for every numberOption.
+for (var i = 0; i < numberOptions.length; i++) {
+
+    // For each iteration, we will create an imageCrystal
+    var crystalImg = $("<img>");
+
+    // First each crystal will be given the class ".crystal-image".
+    // This will allow the CSS to take effect.
+    crystalImg.addClass("crystal-image");
+
+    // Each imageCrystal will be given a src link to the crystal image
+    crystalImg.attr("src", "assets/images/crystal_1.png");
+
+    // Each imageCrystal will be given a data attribute called data-crystalValue.
+    // This data attribute will be set equal to the array value.
+    crystalImg.attr("data-crystalvalue", numberOptions[i]);
+
+    // Lastly, each crystal image (with all it classes and attributes) will get added to the page.
+    $("#crystals").append(crystalImg);
+}
+
+/*********************************
+ *********************************/
+
+// This time, our click event applies to every single crystal on the page. Not just one.
+$(".crystal-image").on("click", function () {
+
+    // Determining the crystal's value requires us to extract the value from the data attribute.
+    // Using the $(this) keyword specifies that we should be extracting the crystal value of the clicked crystal.
+    // Using the .attr("data-crystalvalue") allows us to grab the value out of the "data-crystalvalue" attribute.
+    // Since attributes on HTML elements are strings, we must convert it to an integer before adding to the counter
+
+    var crystalValue = ($(this).attr("data-crystalvalue"));
+    crystalValue = parseInt(crystalValue);
+    // We then add the crystalValue to the user's "counter" which is a global variable.
+    // Every click, from every crystal adds to the global counter.
+    counter += crystalValue;
+
+    // All of the same game win-lose logic applies. So the rest remains unchanged.
+    //alert("New score: " + counter);
+
+    $("#player-number").text(counter);
+
+    if (counter === randomNum) {
+        alert("You win!");
+        wins++;
+        $("#wins").text(wins);
+    }
+
+    else if (counter >= randomNum) {
+        alert("You lose!!");
+        losses++;
+        $("#losses").text(losses);
+    }
+
+
+
+});
